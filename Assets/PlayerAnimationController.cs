bffea3ad98829b27b63ec4bd281cb1e9b63bdfe4
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-05 15:18:47 +0530

    player animations

--- Assets/PlayerAnimationController.cs
+++ Assets/PlayerAnimationController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
 {
+    [SerializeField]
+    public AnimationCurve animationCurve;
+    Animation animations;
+    float curveTime = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animations = GetComponent<Animation>();
+       // animationCurve.Evaluate(curveTime);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 currentPosition = transform.position;
+        currentPosition.y= animationCurve.Evaluate(curveTime);
+        transform.position= currentPosition;
     }
 }
