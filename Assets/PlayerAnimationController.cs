e8b2cc2f897442e39f97724180987f8c17a2e28c
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-06 13:55:21 +0530

    Downloaded assets

--- Assets/PlayerAnimationController.cs
+++ Assets/PlayerAnimationController.cs
@@ -7,9 +7,10 @@ public class PlayerAnimationController : MonoBehaviour
     [SerializeField]
     public AnimationCurve animationCurve;
     Animation animations;
-     public float curveTime = 4f;
+    // public float curveTime = 4f;
      public float playerSpeed = 6f;
-    public Vector3 currentPosition;
+    // public Vector3 currentPosition;
+    public Transform currentTransform;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +22,15 @@ public class PlayerAnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         currentPosition = transform.position;
-        currentPosition.z+=playerSpeed+Time.deltaTime;
-        curveTime= Mathf.Clamp(curveTime, Time.time, 10f);
+        /*currentPosition = transform.position;
+       currentPosition.z+=playerSpeed+Time.deltaTime;
+       curveTime= Mathf.Clamp(curveTime, Time.time, 10f);
 
-        currentPosition.y= animationCurve.Evaluate(curveTime*Time.deltaTime);
-        transform.position= currentPosition;
+       currentPosition.y= animationCurve.Evaluate(curveTime*Time.deltaTime);
+       transform.position= currentPosition;*/
+
+        currentTransform.position += playerSpeed * currentTransform.forward * Time.deltaTime;
+        float valueY= animationCurve.Evaluate(Mathf.PingPong(Time.time,1f));
+        currentTransform.position = new Vector3(currentTransform.position.x, valueY, currentTransform.position.z);
     }
 }
